f32e4400ae9cd1db072f6af2dfcde20581124d3c
dainiuxt <dainiuxt@gmail.com>
2022-05-17 10:28:28 +0300

    add automatic project/issue creator (current user)

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
-from .models import People, Project, Issue
+from .models import People, Project, Issue, ProjectModification, IssueModification
 
 # Register your models here.
 class IssueAdmin(admin.ModelAdmin):
-  list_display = ('summary', 'resolved', 'created_on')
+  list_display = ('summary', 'resolved', 'created_on', 'created_by')
+
+  def save_model(self, request, obj, form, change):
+    obj.created_by = request.user
+    super().save_model(request, obj, form, change) 
+    
+class ProjectAdmin(admin.ModelAdmin):
+  list_display = ('project_name', 'created_on', 'created_by')
+
+  def save_model(self, request, obj, form, change):
+    obj.created_by = request.user
+    super().save_model(request, obj, form, change) 
 
 admin.site.register(People)
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(ProjectModification)
 admin.site.register(Issue, IssueAdmin)
+admin.site.register(IssueModification)
--- tracker/models.py
+++ tracker/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from django.conf import settings
 # Create your models here.
 
 class Project(models.Model):
@@ -11,21 +11,19 @@ class Project(models.Model):
   target_end = models.DateField('Target end date')
   actual_end = models.DateField('Actual end date', null=True, blank=True)
   created_on = models.DateField('Creation date', auto_now_add=True)
-  created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='project_creator')
-  modified_on = models.DateField('Change date', null=True, blank=True)
-  modified_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+  created_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL)
 
   def __str__(self):
     return self.project_name
 
+class ProjectModification(models.Model):
+  project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, blank=True)
+  modified_on = models.DateField('Change date', auto_now_add=True)
+  modified_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
 class People(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
   assigned_project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, blank=True)
-  # created_on = models.DateField('Creation date')
-  # created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='createdby')
-  # modified_on = models.DateField('Change date', null=True, blank=True)
-  # modified_by = models.ManyToManyField(User, related_name='modifiedby', blank=True)
 
   def __str__(self):
     return self.user.username
@@ -39,10 +37,10 @@ class People(models.Model):
   def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
-  def update_profile(request, user_id):
-    user = User.objects.get(pk=user_id)
-    user.profile.bio = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...'
-    user.save()
+  # def update_profile(request, user_id):
+  #   user = User.objects.get(pk=user_id)
+  #   user.profile.bio = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...'
+  #   user.save()
 
   class Meta:
     verbose_name = 'People'
@@ -52,9 +50,10 @@ class People(models.Model):
 class Issue(models.Model):
   summary = models.CharField('Issue summary', max_length=255)
   description = models.TextField('Description', null=True)
-  identified_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+  identified_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='troubleshooter')
   identification_date = models.DateField('Identified on', auto_now_add=True)
   related_project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True)
+
   assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='issue_solver', blank=True)
 
   PIORITY_LIST = (
@@ -70,9 +69,7 @@ class Issue(models.Model):
   actual_resolution = models.DateField('Actual resolution', null=True, blank=True)
   res_summary = models.TextField('Resolution summary', null=True, blank=True)
   created_on = models.DateField('Creation date', auto_now_add=True)
-  created_by = models.ManyToManyField(User, related_name='issue_owner')
-  modified_on = models.DateField('Change date', null=True, blank=True)
-  modified_by = models.ManyToManyField(User, related_name='issue_modifier', blank=True)
+  created_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL, related_name='creator')
 
   def resolved(self):
     if self.actual_resolution is None:
@@ -82,3 +79,8 @@ class Issue(models.Model):
 
   def __str__(self):
     return f"{self.summary}"
+
+class IssueModification(models.Model):
+  issue = models.ForeignKey(Issue, on_delete=models.SET_NULL, null=True, blank=True)
+  modified_on = models.DateField('Change date', auto_now_add=True)
+  modified_by = models.ManyToManyField(User, related_name='issue_modifier', blank=True)
