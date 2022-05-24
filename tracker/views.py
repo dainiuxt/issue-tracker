3d19e8e19179cf5c1dbcae3330465516a0a1950c
dainiuxt <dainiuxt@gmail.com>
2022-05-24 15:43:39 +0300

    add plotly chart

--- tracker/views.py
+++ tracker/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from .models import People, Project, Issue, SoftDeleteQuerySet
+from plotly.offline import plot
+import plotly.graph_objects as go
+import plotly.express as px
 
 def index(request):
     projects = Project.objects.all()
@@ -11,6 +13,26 @@ def index(request):
     issues_active = Issue.objects.all().filter(actual_resolution=None)
     issues_solved = len(issues) - len(issues_active)
 
+    data = [len(issues_active), issues_solved]
+    labels = ['Open', 'Solved']
+    graphs = []
+    graphs.append(
+        go.Pie(values=data, labels=labels)
+    )
+
+    # Setting layout of the figure.
+    layout = {
+        'title': 'Issues stats',
+        'xaxis_title': 'X',
+        'yaxis_title': 'Y',
+        'height': 420,
+        'width': 560,
+    }
+
+    # Getting HTML needed to render the plot.
+    plot_div = plot({'data': graphs, 'layout': layout}, 
+                    output_type='div')
+
     context = {
         'projects': projects,
         'people': people,
@@ -18,6 +40,7 @@ def index(request):
         'issues': issues,
         'issues_active': issues_active,
         'issues_solved': issues_solved,
+        'plot_div': plot_div,
     }
     return render(request, 'index.html', context=context)
 
