7bd2c0deacebdd013669d6bbb7ed412aa015aae7
dainiuxt <dainiuxt@gmail.com>
2022-05-24 13:14:08 +0300

    add google charts to dashboard

--- tracker/views.py
+++ tracker/views.py
@@ -8,15 +8,18 @@ def index(request):
     people = People.objects.all()
     people_active = People.objects.only_active()
     issues = Issue.objects.all()
+    issues_active = Issue.objects.all().filter(actual_resolution=None)
+    issues_solved = len(issues) - len(issues_active)
 
     context = {
         'projects': projects,
         'people': people,
         'people_active': people_active,
-        'issues': issues
+        'issues': issues,
+        'issues_active': issues_active,
+        'issues_solved': issues_solved,
     }
     return render(request, 'index.html', context=context)
-
 
 @login_required
 def people(request):
