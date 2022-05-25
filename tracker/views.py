03a6fea38ca3a073bc986458780273e9dfddbd65
dainiuxt <dainiuxt@gmail.com>
2022-05-25 20:55:28 +0300

    before classbased view for index

--- tracker/views.py
+++ tracker/views.py
@@ -5,15 +5,14 @@ from plotly.offline import plot
 import plotly.graph_objects as go
 import pandas as pd
 
+projects = Project.objects.all()
+people = People.objects.all()
+people_active = People.objects.only_active()
+issues = Issue.objects.all()
+issues_active = Issue.objects.all().filter(actual_resolution=None)
+issues_solved = len(issues) - len(issues_active)
 
 def index(request):
-    projects = Project.objects.all()
-    people = People.objects.all()
-    people_active = People.objects.only_active()
-    issues = Issue.objects.all()
-    issues_active = Issue.objects.all().filter(actual_resolution=None)
-    issues_solved = len(issues) - len(issues_active)
-
     data = [len(issues_active), issues_solved]
     labels = ['Open', 'Solved']
     issues_graphs = []
@@ -21,19 +20,8 @@ def index(request):
         go.Pie(values=data, labels=labels)
     )
 
-    # Setting layout of the figure.
-    issues_layout = {
-        #'title': 'Issues stats',
-        'xaxis_title': 'X',
-        'yaxis_title': 'Y',
-        # 'height': 420,
-        # 'width': 500,
-    }
-
     # Getting HTML needed to render the plot.
-    issues_plot_div = plot({'data': issues_graphs,
-                    'layout': issues_layout}, 
-                    output_type='div')
+    issues_plot_div = plot({'data': issues_graphs}, output_type='div')
 
     projects_data = pd.DataFrame()
     for i in issues:
@@ -45,19 +33,8 @@ def index(request):
         go.Pie(values=projects_data['Item'], labels=projects_data['Project'])
     )
 
-    # Setting layout of the figure.
-    projects_layout = {
-        #'title': 'Issues stats',
-        'xaxis_title': 'X',
-        'yaxis_title': 'Y',
-        # 'height': 420,
-        # 'width': 500,
-    }
-
     # Getting HTML needed to render the plot.
-    projects_plot_div = plot({'data': projects_graphs,
-                    'layout': projects_layout}, 
-                    output_type='div')
+    projects_plot_div = plot({'data': projects_graphs}, output_type='div')
 
     context = {
         'projects': projects,
@@ -71,6 +48,10 @@ def index(request):
     }
     return render(request, 'index.html', context=context)
 
+# @login_required
+# def projects(request):
+
+
 @login_required
 def people(request):
-    return render(request, 'people.html')
+    return render(request, 'people/people.html')
