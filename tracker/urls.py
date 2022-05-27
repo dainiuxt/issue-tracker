98515ace3028ecfa6ea13df6145105787f773b0e
dainiuxt <dainiuxt@gmail.com>
2022-05-27 16:12:28 +0300

    add profile page

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 from tracker.views import (IndexView,
                             ProjectsView,
-                            IssuesView)
+                            IssuesView,
+                            ProfileView)
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('people/', views.people, name='people'),
+    path('people/', ProfileView.as_view(), name='people'),
     path('projects/', ProjectsView.as_view(), name='projects'),
     path('issues/', IssuesView.as_view(), name='issues'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import (ListView, DetailView)
 from .models import People, Project, Issue
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import request
 from plotly.offline import plot
 import plotly.graph_objects as go
 import pandas as pd
@@ -24,7 +25,7 @@ issues_graphs.append(
     go.Pie(values=data, labels=labels)
 )
 # Getting HTML needed to render the plot.
-issues_plot_div = plot({'data': issues_graphs}, output_type='div')
+issues_status_div = plot({'data': issues_graphs}, output_type='div')
 
 projects_data = pd.DataFrame()
 for i in issues:
@@ -51,7 +52,7 @@ class IndexView(ListView):
         context['issues'] = issues
         context['issues_active'] = issues_active
         context['issues_solved'] = issues_solved
-        context['issues_plot_div'] = issues_plot_div
+        context['issues_status_div'] = issues_status_div
         context['projects_plot_div'] = projects_plot_div
         context['index_menu_active'] = 'active'
         return context
@@ -100,16 +101,48 @@ class IssuesView(LoginRequiredMixin, ListView):
         # Getting HTML needed to render the plot.
         plot_div = plot({'data': issues_graphs}, output_type='div')
 
+        issues_by_project = []
+        issues_by_project.append(
+            go.Pie(values=issues_data['Item'], labels=issues_data['Project'])
+        )
+        # Getting HTML needed to render the plot.
+        issues_by_proj_div = plot({'data': issues_by_project}, output_type='div')
+
         context = super(IssuesView, self).get_context_data(**kwargs)
         context['issues_menu_active'] = 'active'
         context['issues'] = issues
         context['issues_active'] = issues_active
         context['issues_solved'] = issues_solved
         context['plot_div'] = plot_div
+        context['issues_by_proj_div'] = issues_by_proj_div
         return context
 
+class ProfileView(ListView, LoginRequiredMixin):
+    model = People
+    template_name = 'people/people.html'
+    queryset = People.objects.all()
 
+    def get_context_data(self, **kwargs):
 
-@login_required
-def people(request):
-    return render(request, 'people/people.html')
+        people = People.objects.all()
+        # user = People.objects.all().filter(pk=request.user.id)
+        projects = Project.objects.all().filter(assigned_to=self.request.user.id)
+        people_active = People.objects.only_active()
+        issues = Issue.objects.all()
+        issues_active = Issue.objects.all().filter(actual_resolution=None)
+        issues_solved = []
+        for i in issues:
+            if i.actual_resolution != None:
+                issues_solved.append(i)
+
+        context = super(ProfileView, self).get_context_data(**kwargs)
+        context['projects'] = projects
+        context['people'] = people
+        context['people_active'] = people_active
+        context['issues'] = issues
+        context['issues_active'] = issues_active
+        context['issues_solved'] = issues_solved
+        context['issues_status_div'] = issues_status_div
+        context['projects_plot_div'] = projects_plot_div
+        context['people_menu_active'] = 'active'
+        return context
