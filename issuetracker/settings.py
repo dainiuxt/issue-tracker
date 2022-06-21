8f72eb57cc003367373c1fec45ae863c1614290a
dainiuxt <dainiuxt@gmail.com>
2022-06-21 14:44:46 +0300

    add heroku conf. heroku local runs OK

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+# import django_heroku
+# django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# PROJECT_ROOT = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,7 +124,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = 'tracker/static/'
+
+# STATICFILES_DIRS = [
+#     os.path.join(PROJECT_ROOT, 'static'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
