f6c2ead08d29b301fbf05cc88bec6243e726a930
dainiuxt <dainiuxt@gmail.com>
2022-06-21 14:55:32 +0300

    move secret_key to .env

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 import os
 # import django_heroku
 # django_heroku.settings(locals())
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j#$wt&&4)6y=j*s90s&*-1#g7^j0a_#7v6%v^yr72iu^ksyltu'
+# SECRET_KEY = 'django-insecure-j#$wt&&4)6y=j*s90s&*-1#g7^j0a_#7v6%v^yr72iu^ksyltu'
+
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -124,12 +129,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = 'tracker/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(PROJECT_ROOT, 'static'),
-# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
